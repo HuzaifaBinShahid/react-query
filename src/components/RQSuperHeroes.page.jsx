e3fd8c43473e979f76e5b0d0c9eb7e44f471fc52
Huzaifa <shuzafa222@gmail.com>
2024-08-21 15:09:28 +0500

    custom query hook

--- src/components/RQSuperHeroes.page.jsx
+++ src/components/RQSuperHeroes.page.jsx
@@ -1,18 +1,23 @@
 import React from "react";
-import { useQuery } from "react-query";
-import axios from "axios";
-
-const fetchSuperHeroes = () => {
-  return axios.get("http://localhost:4000/superheroes");
-};
+import { useSuperHeroesData } from "../hooks/SuperHeroData";
 
 const RQSuperHeroes = () => {
-  const { isLoading, data, isError, error } = useQuery(
-    "superheroes",
-    fetchSuperHeroes
+  // const [fetchHeroes, setFetchHeroes] = useState(false);
+
+  const onSuccess = (data) => {
+    console.log("Performed Fetching and is success", data);
+  };
+
+  const onError = (error) => {
+    console.log("Got an error while fetching", error);
+  };
+
+  const { isLoading, data, isError, error, isFetching } = useSuperHeroesData(
+    onSuccess,
+    onError
   );
 
-  if (isLoading) {
+  if (isLoading || isFetching) {
     return <h2>Loading...</h2>;
   }
 
@@ -20,14 +25,20 @@ const RQSuperHeroes = () => {
     return <h2>{error.message}</h2>;
   }
 
+  // const onClickHandle = () => {
+  //   setFetchHeroes(true);
+  // };
+
   return (
-    <div>
+    <>
       <h2>RQ Super Heroes List</h2>
-      {data?.data.map((hero) => {
+      <button>Fetch Heroes</button>
+      {/* {data?.data.map((hero) => {
         console.log(hero);
         return <div key={hero.name}> {hero.name} </div>;
-      })}
-    </div>
+      })} */}
+      {data && data.map((heroName) => <div key={heroName}>{heroName}</div>)}
+    </>
   );
 };
 
---
+++ src/hooks/SuperHeroData.jsx
@@ -0,0 +1,20 @@
+import { useQuery } from "react-query"
+import axios from "axios"
+
+const fetchSuperHeroes = () =>{
+   return axios.get('http://localhost:4000/superheroes')
+}
+
+export const useSuperHeroesData = (onSuccess,onError) =>{
+    return useQuery('superhero', fetchSuperHeroes, {
+        onSuccess,
+        onError,
+        // enabled,
+        // refetchOnMount: false,
+        // refetchOnWindowFocus: false,
+        select: (data) =>{
+            const superHeroNames = data.data.map((hero) => hero.name)
+            return superHeroNames;
+        }
+    })
+}
